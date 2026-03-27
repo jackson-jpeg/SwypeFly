9b5a127b505bb530409768164a562ffadfbcd01f
Eevee <eevee@aimpov.com>
2026-03-27 01:50:49 +0000

    wip: changes from VPS

--- SoGoJet-iOS/Package.swift
+++ SoGoJet-iOS/Package.swift
@@ -4,9 +4,15 @@ import PackageDescription
 let package = Package(
     name: "SoGoJet",
     platforms: [.iOS(.v17)],
+    dependencies: [
+        .package(url: "https://github.com/stripe/stripe-ios.git", from: "24.0.0"),
+    ],
     targets: [
         .executableTarget(
             name: "SoGoJet",
+            dependencies: [
+                .product(name: "StripePaymentSheet", package: "stripe-ios"),
+            ],
             path: "SoGoJet"
         )
     ]
